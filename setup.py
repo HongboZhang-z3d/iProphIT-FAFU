ec096dea58899b765e5b3bd79727f93daa77a785
HongboZhang-z3d <18842255601@163.com>
2026-07-08 11:19:32 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='iprophit',
-    version='1.0.0', 
+    version='1.0.1', 
     packages=find_packages(),
     python_requires='>=3.12', 
     
